35c823e411becab017545b7a48fdc24de9372dab
Prince Reyes <krayzerprince@gmail.com>
2023-08-16 10:15:36 +0800

    added data and information

--- README.md
+++ README.md
@@ -1,8 +1,101 @@
-# React + Vite
+# Position Size Calculator
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Introduction
 
-Currently, two official plugins are available:
+Position Size Calculator is a powerful tool designed for derivatives traders in the crypto and forex markets. This tool helps you manage your risk effectively and determine optimal position sizes, enabling you to make informed trading decisions.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Features
+
+- **Risk Calculation:** Estimate potential risk based on account size, leverage, risk percentage, and stop loss percentage.
+- **Position Size Estimation:** Determine the optimal position size aligned with your risk tolerance and trading parameters.
+- **Forex Lot Sizing:** Calculate forex lot sizes consistently for your trading operations.
+
+## How to Use
+
+- **Account Size:** Enter your trading account size.
+- **Leverage:** Specify your chosen leverage.
+- **Risk Percentage:** Indicate the percentage of your account you're willing to risk per trade.
+- **Stop Loss Percentage:** Define the percentage at which your stop loss will trigger.
+- **Calculate:** Click the "Calculate" button to receive risk, position size, and forex lot size.
+
+## Pro Tips
+
+- Maintain accurate inputs for precise calculations.
+- Experiment with different risk scenarios and position sizes before executing trades.
+- Regularly revisit the app to adapt your trading strategy based on market changes.
+
+## Getting Started
+
+Follow these steps to get the app up and running:
+
+### Prerequisites
+
+- Node.js: Make sure you have Node.js installed. You can download it from [here](https://nodejs.org/).
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/position-size-calculator.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd position-size-calculator
+   ```
+
+3. Install the dependencies:
+   ```bash
+   npm install
+   ```
+
+## Usage/Examples
+
+Usage
+Open your terminal and navigate to the project directory.
+
+Start the development server:
+
+```bash
+npm run dev
+```
+
+Open your web browser and go to http://localhost:5173/ to access the app.
+
+```
+VITE v4.4.9  ready in 1861 ms
+
+  ➜  Local:   http://localhost:5173/
+  ➜  Network: use --host to expose
+  ➜  press h to show help
+```
+
+Input your trading parameters (account size, leverage, risk percentage, and stop loss percentage) and click the "Calculate" button.
+
+The app will display your risk, position size, and forex lot size based on the provided inputs.
+
+## Tech Stack
+
+**React + Vite:** Frontend JavaScript library
+
+**CSS:** Styling
+
+**Node.js:** Backend JavaScript runtime
+
+## Contributing
+
+Contributions are always welcome!
+
+Contributions are welcome! If you'd like to contribute, please fork the repository and
+
+`create a pull request.`
+
+## Feedback
+
+If you encounter any issues or have suggestions, feel free to open an issue. Your feedback is valuable to us!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,24 @@ export default function App() {
   const [riskPercentage, setRiskPercentage] = useState("");
   const [stopLoss, setStopLoss] = useState("");
 
+  function formatCurrency(value) {
+    const numericValue = parseFloat(value);
+    if (!isNaN(numericValue)) {
+      return numericValue.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+    }
+    return "";
+  }
+
+  const formattedAccountSize = formatCurrency(accountSize);
+  const formattedLossValue = formatCurrency(posSize.lossValue);
+  const formattedTradeMargin = formatCurrency(posSize.tradeMargin);
+  const formattedPosSize = formatCurrency(posSize.positionSize);
+
   function calculate() {
     if (!accountSize || !leverage || !riskPercentage || !stopLoss) {
       // If any of the input fields are empty, show a message and return
@@ -61,20 +79,38 @@ export default function App() {
         <h1>Position Size Calculator</h1>
         <div className="result">
           <h2>Your Position Size</h2>
+          <div className="account-size">
+            <p className="result-p">Account Size</p>
+            <span>{formattedAccountSize}</span>
+          </div>
+          <div className="inputValue">
+            <p className="result-p">
+              Leverage
+              <span> {leverage}x</span>
+            </p>
+            <p className="result-p">
+              Stoploss
+              <span> {stopLoss}%</span>
+            </p>
+            <p className="result-p">
+              Risk Per Trade
+              <span> {riskPercentage}%</span>
+            </p>
+          </div>
           <div className="numbers">
             {!posSize.isCorrect ? (
               "Enter your position"
             ) : (
               <>
                 <p>
-                  Loss value:{" "}
-                  <span style={{ color: "red" }}>$ {posSize.lossValue}</span>
+                  Loss value:
+                  <span style={{ color: "red" }}>{formattedLossValue}</span>
                 </p>
                 <p>
-                  Trade Margin: <span>$ {posSize.tradeMargin} </span>
+                  Trade Margin: <span>{formattedTradeMargin} </span>
                 </p>
                 <p>
-                  Position Size: <span>$ {posSize.positionSize} </span>
+                  Position Size: <span>{formattedPosSize} </span>
                 </p>
                 <p>
                   Forex Lot Size: <span>{posSize.forexLotSize} lot</span>
@@ -85,32 +121,46 @@ export default function App() {
         </div>
         <div className="controls">
           <form onSubmit={(e) => e.preventDefault()}>
-            <input
-              type="number"
-              placeholder="Account Size"
-              value={accountSize}
-              onChange={(e) => setAccountSize(e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Leverage"
-              value={leverage}
-              onChange={(e) => setLeverage(e.target.value)}
-            />
-
-            <input
-              type="number"
-              placeholder="Stop Loss %"
-              value={stopLoss}
-              onChange={(e) => setStopLoss(e.target.value)}
-            />
-
-            <input
-              type="number"
-              placeholder="Risk %"
-              value={riskPercentage}
-              onChange={(e) => setRiskPercentage(e.target.value)}
-            />
+            <div className="border-input">
+              <label htmlFor="account-size">Account Size</label>
+              <input
+                id="account-size"
+                type="number"
+                placeholder="e.g: ($) 1060.43 "
+                value={accountSize}
+                onChange={(e) => setAccountSize(e.target.value)}
+              />
+            </div>
+            <div className="border-input">
+              <label htmlFor="leverage">Leverage</label>
+              <input
+                id="leverage"
+                type="number"
+                placeholder="e.g: 125 (x)"
+                value={leverage}
+                onChange={(e) => setLeverage(e.target.value)}
+              />
+            </div>
+            <div className="border-input">
+              <label htmlFor="stop-loss">Stop Loss</label>
+              <input
+                id="stop-loss"
+                type="number"
+                placeholder="e.g: 0.57 (%)"
+                value={stopLoss}
+                onChange={(e) => setStopLoss(e.target.value)}
+              />
+            </div>
+            <div className="border-input">
+              <label htmlFor="risk-percent">Risk Percentage</label>
+              <input
+                id="risk-percent"
+                type="number"
+                placeholder="e.g: 1 - 5 value at risk "
+                value={riskPercentage}
+                onChange={(e) => setRiskPercentage(e.target.value)}
+              />
+            </div>
             <button onClick={calculate}>Calculate</button>
             {posSize.isCorrect && (
               <button
@@ -120,7 +170,8 @@ export default function App() {
                   setRiskPercentage("");
                   setStopLoss("");
                   setPosSize(!posSize.isCorrect);
-                }}>
+                }}
+              >
                 Reset
               </button>
             )}
